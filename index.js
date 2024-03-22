729cea8c2378db7e973e8f6fb4d3a1559ce2f068
Margarita Ramirez <margarita26.mr@gmail.com>
2024-03-22 10:43:33 -0400

    add buttons for countries

--- index.js
+++ index.js
@@ -7,21 +7,41 @@ function closePopup(){
     popup.classList.remove("open-popup");
 }
 
-fetch("http://localhost:3000/countries")
-.then(response => response.json())
-.then(countries => {
-    const nameOfCountry = document.querySelector("#nameOfCountry");
-    countries.forEach(country => {
-        const span = document.createElement("span");
-        span.textContent = country.name;
-        span.addEventListener("click", () => {displayCountriesDetails(country);});
-        nameOfCountry.appendChild(span);
-    });
-    // displayCountriesDetails();
-});
+// fetch("http://localhost:3000/countries")
+// .then(response => response.json())
+// .then(countries => {
+//     const btnContainer = document.querySelector("#countries");
+//     countries.forEach(country => {
+//         const button = document.createElement("button");
+//         button.textContent = country.name;
+//         button.addEventListener("click", () => {displayCountriesDetails(country);});
+//         btnContainer.appendChild(button);
+//     });
+//     displayCountriesDetails();
+// });
 
+fetch("http://localhost:3000/countries")
+    .then(response => response.json())
+    .then(countries => {
+        const btnContainer = document.querySelector("#newButtons");
+        countries.forEach(country => {
+            const button = document.createElement("button");
+            button.textContent = country.name;
+            button.addEventListener("click", () => {
+                displayCountriesDetails(country);
+            });
+            btnContainer.appendChild(button);
+        });
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
+
+// add a class to the html and add that to the fetch.
+//htne when us the class to edit something on css.
 
 function displayCountriesDetails(country){
+    document.querySelector("#countrieyName").textContent = country.name;
     document.querySelector("#countries-capital").textContent = country.capital;
     document.querySelector("#countries-language").textContent = country.language;
     document.querySelector("#countries-tipping").textContent = country.tipping;
